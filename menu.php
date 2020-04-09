1add9cbe10c1ea2489827b9f3f76d459074b900b
Mrmachete <mache9v9@gmail.com>
2020-04-08 19:55:34 -0500

    my primer commit

---
+++ menu.php
@@ -0,0 +1,301 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/menu.js" type="text/javascript"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <title>Software solutions</title>
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Software Solutions</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="mision.php">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="servicio.php">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contactos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <header>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <!-- Slide One - Set the background image for this slide in the line below -->
+                    <div class="carousel-item active" style="background-image: url('https://3.bp.blogspot.com/-_7vaKiNZPXk/XCWoF-0xj7I/AAAAAAAAAMk/Bx7Ne5WLEvINHPDoG1jwY6rGO2d62pprwCKgBGAs/s1600/ux-design.jpeg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-4">First Slide</h2>
+                            <p class="lead">This is a description for the first slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Two - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('https://4.bp.blogspot.com/-InDD3Hm_bhU/XB4_TK3TT7I/AAAAAAAAAJ4/r5tUeCOqq1MTchFh7D7pWdf582A4qYIIwCEwYBhgL/s1600/Businesswoman-working-at-a-computer-1280x720.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-4">Second Slide</h2>
+                            <p class="lead">This is a description for the second slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Three - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('https://1.bp.blogspot.com/-GWl5F8P4t-8/XB4_VV4VE5I/AAAAAAAAAKE/SmHRzas-LpMpDBoLn6otR9AznVsr6L7OgCEwYBhgL/s1600/t1larg.africa.cnn.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-4">Third Slide</h2>
+                            <p class="lead">This is a description for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </header>
+
+        <!-- Page Content -->
+        <section class="py-5 text-center">
+            <div class="container"> 
+                <h2 class="text-center">Software  Servicios</h2>
+                <p class="text-muted mb-5 text-center">Aqui encontraran las soluciones que tus ideas necesitan.</p>
+                <div class="row">
+                    <div class="col-sm-6 col-lg-4 mb-3">
+                        <svg class="lnr text-primary services-icon">
+                        <use xlink:href="#lnr-magic-wand"></use>
+                        </svg>
+                        <h6>Integridad</h6>
+                        <p class="text-muted">Simple integracion de suspartes en el desarrollo de software.</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 mb-3">
+                        <svg class="lnr text-primary services-icon">
+                        <use xlink:href="#lnr-heart"></use>
+                        </svg>
+                        <h6>Damos vida a tus ideas </h6>
+                        <p class="text-muted">Le ponemos corazon a tus ideas para darle vida.</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 mb-3">
+                        <svg class="lnr text-primary services-icon">
+                        <use xlink:href="#lnr-rocket"></use>
+                        </svg>
+                        <h6>Somos pioneros en velocidad</h6>
+                        <p class="text-muted">Tus registros estaran simpre disponibles, pero numca olividamos la seguridad.</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 mb-3">
+                        <svg class="lnr text-primary services-icon">
+                        <use xlink:href="#lnr-paperclip"></use>
+                        </svg>
+                        <h6>Contamos con respaldos</h6>
+                        <p class="text-muted">Contamos con multiples plataformas de espejos que nos sirve como respaldo de y seguridad, ante cualquir eventualidad</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 mb-3">
+                        <svg class="lnr text-primary services-icon">
+                        <use xlink:href="#lnr-screen"></use>
+                        </svg>
+                        <h6>Control Remoto</h6>
+                        <p class="text-muted">Nuestro equipo esta entrenado para solucionar los equipos remotamente.</p>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 mb-3">
+                        <svg class="lnr text-primary services-icon">
+                        <use xlink:href="#lnr-inbox"></use>
+                        </svg>
+                        <h6>Base de datos</h6>
+                        <p class="text-muted">La integridad en nuestras adminitracion se adaptada a cada una de las necesidades, que tus ideas requieran.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="main">
+            <div class="container mt-4">
+                <h1 class="text-center mb-4 p-4 text-secondary">Software Herramientas</h1>
+                <div class="row">
+
+                    <div class="card-columns">
+                        <div class="card shadow border-0">
+                            <img class="card-img-top" src="imagenes/raspberry-pi-zero-w-1.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Raspberry pi</h5>
+                                <p class="card-text">Raspberry Pi es un ordenador de placa reducida, ordenador de
+                                    placa única u ordenador de placa simple (SBC) de bajo coste
+                                    desarrollado en el Reino Unido por la Raspberry Pi Foundation,
+                                    con el objetivo de estimular la enseñanza de informática en las
+                                    escuelas.​ El modelo original se convirtió en más popular de lo
+                                    que se esperaba, hasta incluso vendiéndose afuera del mercado
+                                    objetivo para usos como robótica. No incluye periféricos (como
+                                    teclado y ratón) o carcasa.</p>
+                            </div>
+                        </div>
+                        <div class="card shadow border-0  p-3">
+                            <blockquote class="blockquote mb-0 card-body">
+                                <img class="card-img" src="imagenes/INSTALAR-CARCASA-RASPBERRY-PI_1.jpg"  alt="Card image">
+                                <p>Los manuales son entendibles para una facil instalacion.</p>
+                                <footer class="blockquote-footer">
+                                    <small class="text-muted">
+                                       Like <cite title="Source Title"></cite>
+                                    </small>
+                                </footer>
+                            </blockquote>
+
+                        </div>
+                        <div class="card shadow border-0">
+                            <img class="card-img-top" src="imagenes/BD3.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Base de Datos</h5>
+                                <p class="card-text">Los Administradores de Bases de Datos son responsables del manejo, mantenimiento, desempeño y de la confiabilidad de bases de datos. Asimismo, están a cargo de la mejora y diseño de nuevos modelos de las mismas.
+
+                                    Manejar una base de datos implica recolectar, clasificar y resguardar la información de manera organizada, por ello, estos profesionales velan por garantizar que la misma esté debidamente almacenada y segura, además de que sea de fácil acceso cuando sea necesario.
+                                </p>
+                                <p class="card-text"><small class="text-muted"></small></p>
+                            </div>
+                        </div>
+                        <div class="card shadow border-0 bg-primary text-white text-center p-3">
+                            <blockquote class="blockquote mb-0">
+                                <img class="card-img" src="imagenes/logo2.jpg" alt="Card image">
+                                <p>La creación final llamado Bender es la unión de diferentes tecnologías.</p>
+                                <footer class="blockquote-footer">
+                                    <small>
+                                        <cite title="Source Title"></cite>
+                                    </small>
+                                </footer>
+                            </blockquote>
+                        </div>
+                        <div class="card shadow border-0 text-center">
+                            <div class="card-body">
+                                <h5 class="card-title">Union De Partes</h5>
+                               
+                            </div>
+                        </div>
+                        <div class="card shadow border-0">
+                            
+                           
+                        </div>
+
+                        <div class="card shadow border-0">
+                            
+                            
+                        </div>
+                         <div class="card shadow border-0">
+                            <img class="card-img-top"  src="imagenes/software.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Software</h5>
+                                <p class="card-text">Un programa informático es el resultado final de un proceso que empieza con el planteamiento de un problema. Un programa es un conjunto de instrucciones escritas con un lenguaje de programación y que serán ejecutadas en un ordenador.
+Cada PC reconoce un lenguaje propio, denominado lenguaje máquina o ensamblador, que consta de unas instrucciones muy a bajo nivel, en las cuales se accede directamente a los elementos físicos de la máquina como por ejemplo los registros.
+</p>
+                            </div>
+                            <img alt=""/>
+                        </div>
+                       
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Header -->
+        <header class="bg-primary text-center py-5 mb-4">
+            <div class="container">
+                <h1 class="font-weight-light text-white">Conoce el Equipo</h1>
+            </div>
+        </header>
+
+        <!-- Page Content -->
+        <div class="container">
+            <div class="row">
+                <!-- Team Member 1 -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-0 shadow">
+                        <img src="imagenes/jonatan.png" class="card-img-top" alt="...">
+                   
+                        <div class="card-body text-center">
+                            <h5 class="card-title mb-0">Team Member</h5>
+                            <div class="card-text text-black-50">Jonatan Castaño</div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Team Member 2 -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-0 shadow">
+                        
+                        <img src="imagenes/velazco.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title mb-0">Team Member</h5>
+                            <div class="card-text text-black-50">Victor Guevara</div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Team Member 3 -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-0 shadow">
+                         
+                        <img src="imagenes/jair.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title mb-0">Team Member</h5>
+                            <div class="card-text text-black-50">Jair Plummer</div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Team Member 4 -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-0 shadow">
+                     
+                        <img src="imagenes/velazco1.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title mb-0">Team Member</h5>
+                            <div class="card-text text-black-50">Carlos Velasco</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+
+            </div>
+
+        </div>
+        <!-- /.container -->
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://cdn.linearicons.com/free/1.0.0/svgembedder.min.js"></script>
+
+    </body>
+</html>
---
+++ mision.php
@@ -0,0 +1,82 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="css/mision.css" rel="stylesheet" type="text/css"/>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/menu.js" type="text/javascript"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Software Solutions</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="mision.php">Nosotros</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="servicio.php">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  </div>
+
+<div class="how-section1">
+                    <div class="row">
+                        <div class="col-md-6 how-img">
+                            <img src="" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                        <div class="col-md-6">
+                            <h4></h4>
+                                        <h4 class="subheading"></h4>
+                        <p class="text-muted"></p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>VISION.</h4>
+                                        <h4 class="subheading">Ser una empresa reconocida.</h4>
+                                        <p class="text-muted">posicionarnos a nivel nacional por su excelente calidad de nuestro
+                                            producto el software s10 ERP, superando las expectativas de nuestros clientes y un grado maximo de satisfaccion por el producto.</p>
+                        </div>
+                        <div class="col-md-6 how-img">
+                            <img src="https://image.ibb.co/cHgKnU/Work_Section2_freelance_img2.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 how-img">
+                             <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                        <div class="col-md-6">
+                            <h4>MISION.</h4>
+                                        <h4 class="subheading">Fabricamos y brindamos al mercado nacional productos originales.</h4>
+                                        <p class="text-muted">Creados por los mejores programadores de software del pais, con los mas altos estandares de calidad, asimismo respetando, conservando el medio ambiente y utilizando adecuadamente la tecnologia, 
+                                            cumpliendo las normas que generan confianza, satisfaccion a nuestros clientes y alta rentabilidad..</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>VALORES.</h4>
+                                        <h4 class="subheading">Trabajar en equipo y comunicarnos respetuosamente provocando un intercambio y una sinergia que enriquece, una transmision de conocimientos y creencias, y una mayor efectividad en el logro de los objetivos personales y organizacionales.</h4>
+                                        <p class="text-muted">Sentirse en familia formando parte de la organizacion. Estar orgulloso del lugar de trabajo. Cuidar los recursos de nuestra empresa.</p>
+                        </div>
+                        <div class="col-md-6 how-img">
+                            <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt=""/>
+                        </div>
+                    </div>
+                </div>
